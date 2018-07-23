485dcf3c61ae5e0ab1f25b0a0b388e6089409f88
vantoniobta <vale.residencia@gmail.com>
2018-07-23 18:58:35 -0500

    nav-card

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -3,13 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navigation from './components/Navigation';
+import { tasks } from  './tasks.json';
+
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      tasks
+    }
+  }
+
+
   render() {
+    const tasks = this.state.tasks.map((taks, i) =>{
+      return (
+        <div className="cold-md-4">
+            <div className="card mt-4">
+              <div className="card-header">
+               <h3>{taks.title}</h3>
+              </div>
+              <div className="card-body">
+              <p>{taks.usuarios}</p>
+              </div>
+            </div>
+          </div>
+        )
+   })
+
     return (
       <div className="App">
         <Navigation title="MMDigital"/>
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="container">
+            <div className="row mt-4">
+              { tasks}
+            </div>
+          </div>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
--- app-react/src/components/Navigation.js
+++ app-react/src/components/Navigation.js
@@ -4,7 +4,7 @@ class Navigation extends Component {
 	render(){
 		return (
 		     <nav className="navbar navbar-dark bg-dark">
-	           <a heref="#" className="text-white">
+	           <a href="#" className="text-white">
 	           { this.props.title}
 	           </a>
 	         </nav>
