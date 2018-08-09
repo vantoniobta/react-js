fa881a83b653bca052d4dd8200f8113c89409dc0
vantonio <vale.residencia@gmail.com>
2018-08-09 18:37:25 -0500

    link path route

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import './nav.css';
 
-import Navigation from './components/Navigation';
-import Header     from './components/Header';
-import Strategy   from './components/Strategy';
+import Home from './Home';
+import About from './About';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Navigation/>
-         <Header/>
-         <Strategy/>
+	     <nav>
+		    <div className="menu-icon">
+              <i className="fa fa-bars fa-2x"></i>
+		    </div>
+ 	         <div className="logo">
+		          <img src="http://agencia.multimedios.tv/img/toolbar_logo.png" width="60%" alt="logo_img"/>
+		     </div>
+		      <div className="menu">
+		         <ul>
+		            <li><NavLink exact to="/">Advertising</NavLink></li>
+		            <li><NavLink to="/about">Tarifario</NavLink></li>
+		         </ul>
+		       </div>
+	       </nav>
+
+	        <header>
+		      <div className="example">
+			       <p>Multimedios</p>
+				        <ul className="list-name">
+						   <li className="home-title">Monterrey</li>
+						   <li className="home-title">Cd.Mexico</li>
+		                   <li className="home-title">Costa Rica</li>
+						   <li className="home-title">Laguna</li>
+						</ul>
+			    </div>
+			</header>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route  path="/about" component={About} />
+          </Switch>
+
       </div>
     );
   }
