c10905379284f599c416a0308a31dfc4451fc9fe
vantonio <vale.residencia@gmail.com>
2018-08-13 23:56:35 -0500

    dependency

--- app-react/src/components/Socialmedia.js
+++ app-react/src/components/Socialmedia.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Socialmedia = () => {
 	return (
 		<div className="container">
-               <h3>Redes Sociales</h3>
+               <h3>Redes Sociales 123</h3>
             </div>
 		)
 }
